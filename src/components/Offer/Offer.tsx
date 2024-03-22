28c3c896efa180837215253d1103cb08c26b82ee
Behnam Hamidi <eslp91@hotmail.com>
2024-03-22 10:44:54 +0330

    go

--- src/components/Offer/Offer.tsx
+++ src/components/Offer/Offer.tsx
@@ -1,12 +1,28 @@
+"use client";
 import "./offer.css";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
 
 const Offer = () => {
+  const [offerOpen, setOfferOpen] = useState(true);
+
   return (
-    <div className="offer">
+    <div className={offerOpen ? "offer" : "offerClosed"}>
+      <button
+        className="offerCloseBtn"
+        onClick={() => setOfferOpen(!offerOpen)}
+      >
+        <IoClose />
+      </button>
       <p className="offText">
         50% خفیف ویژه به مناسبت ایام نوروز روی تمامی دوره های سایت
       </p>
-      <div className="offCountdown">00:00:00</div>
+      <div className="offCountdown">
+        <span className="offCountdown-days">00</span>:
+        <span className="offCountdown-hours">00</span>:
+        <span className="offCountdown-minutes">00</span>:
+        <span className="offCountdown-seconds">00</span>
+      </div>
     </div>
   );
 };
