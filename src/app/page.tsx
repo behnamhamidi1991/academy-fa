7ef90855e868f6c909886d4e1107ab4264334be3
Behnam Hamidi <eslp91@hotmail.com>
2024-03-22 13:26:20 +0330

    slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Courses from "@/components/Homepage/Courses/Courses";
+import Slider from "@/components/Homepage/Slider/Slider";
 import React from "react";
 
 const Home = () => {
   return (
     <main>
+      <Slider />
       <Courses />
     </main>
   );
