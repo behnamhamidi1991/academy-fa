469bd3d34dc4caa168843ab27ce948ee594eb497
Behnam Hamidi <eslp91@hotmail.com>
2024-03-22 10:17:37 +0330

    header closed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,36 +3,41 @@ import { FaRegUser } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
 import Image from "next/image";
 import Logo from "@/assets/icons/acicon.png";
+import Link from "next/link";
+import Navbar from "../Navbar/Navbar";
 
 const Header = () => {
   return (
-    <header>
+    <header className="header">
       <div className="headerTop">
+        {/* Right */}
         <div className="headerTop-right">
           <div className="headerTop-right-logo">
             <Image src={Logo} width={40} height={40} alt="logo" />
             <p>آکادمی آموزش آنلاین دیپ</p>
           </div>
           <div className="headerTop-right-searchbar">
-            <input
-              type="text"
-              placeholder="دوره مورد نظر خود را جستوجو کنید ..."
-            />
+            <form>
+              <input
+                type="text"
+                placeholder="دوره مورد نظر خود را جستوجو کنید ..."
+              />
+              <button className="headerTop-right-searchbar-btn">جستوجو</button>
+            </form>
           </div>
-          div.
         </div>
+        {/* Left */}
         <div className="headerTop-left">
           <div className="headerTop-left-user">
             <FaRegUser className="header-user-icon" />
           </div>
           <div className="headerTop-left-cart">
+            <span className="cartQuantity">1</span>
             <FaCartShopping className="header-cart-icon" />
           </div>
         </div>
       </div>
-      <div className="headerBottom">
-        <ul className="headerNavbar"></ul>
-      </div>
+      <Navbar />
     </header>
   );
 };
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul className="navLinks">
+        <li>
+          <Link href="/" className="navLink">
+            صفحه اصلی
+          </Link>
+        </li>
+        <li>
+          <Link href="/" className="navLink">
+            دسته بندی
+          </Link>
+        </li>
+        <li>
+          <Link href="/" className="navLink">
+            وبلاگ
+          </Link>
+        </li>
+        <li>
+          <Link href="/" className="navLink">
+            دباره ما
+          </Link>
+        </li>
+        <li>
+          <Link href="/" className="navLink">
+            تماس با ما
+          </Link>
+        </li>
+        <li>
+          <Link href="/" className="navLink">
+            همکاری با ما
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
