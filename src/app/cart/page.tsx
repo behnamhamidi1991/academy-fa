3d58736d5842f018abf2a36259ce12c382bb1f2a
Behnam Hamidi <eslp91@hotmail.com>
2024-03-23 17:26:33 +0330

    cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 import { FaShoppingCart } from "react-icons/fa";
 import "./cart.css";
+import { courses } from "@/data";
+import Image from "next/image";
 
 const CartPage = () => {
+  const quantity = courses.length;
+
   return (
     <div className="cartPage">
       <div className="cartPage-header">
@@ -10,7 +14,31 @@ const CartPage = () => {
         <h2>سبد خرید</h2>
         <p>سبد خرید شما خالی است</p>
       </div>
-      <div className="cartPage-body"></div>
+      <div className="cartPage-body">
+        <div className="cartItems-container">
+          {courses.map((item, index) => (
+            <div key={item.id} className="cartItems-box">
+              <div className="cartItems-number">{index + 1}</div>
+              <div className="cartItems-image-title">
+                <h2>{item.title}</h2>
+                <Image
+                  src={item.image}
+                  alt={item.title}
+                  className="cartItems-box-image"
+                />
+              </div>
+              <div className="cartItems-price">
+                <p>قیمت: {item.price.toLocaleString()}</p>
+                <p>قیمت با تخفیف: {item.offPrice.toLocaleString()}</p>
+              </div>
+              <div className="cartItems-btns">
+                <button>حذف آیتم</button>
+                <button>ادامه و پرداخت</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
