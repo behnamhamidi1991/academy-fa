252fc24578b679224a49f851c68d62f6774bbd03
Behnam Hamidi <eslp91@hotmail.com>
2024-03-31 13:11:55 +0330

    fonts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import "./header.css";
-import logo from "../../assets/icons/acicon.png";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { toggle } from "../../features/themeSlice/themeSlice";
+import logo from "../../assets/icons/acicon.png";
+import sunIcon from "../../assets/icons/sun.svg";
+import moonIcon from "../../assets/icons/moon.svg";
 import { FaShoppingCart } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { MdKeyboardArrowUp } from "react-icons/md";
 
 const Header = () => {
+  const theme = useSelector((state) => state.theme.dark);
+  const dispatch = useDispatch();
+
   return (
     <div className="headerWrapper">
-      <header className="header">
+      <header className={theme ? "header dark" : "header light"}>
         <div className="logoAndLinks">
           <div className="headerLogo">
-            <img src={logo} alt="" />
+            <img src={logo} alt="logo" />
             <p>آکادمی آموزشی دیپ لرن</p>
           </div>
           <ul className="headerLinks">
@@ -21,7 +30,16 @@ const Header = () => {
               </Link>
             </li>
             <li>
-              <button>منوی سایت</button>
+              <button className="headerLinkBtn">
+                <MdKeyboardArrowDown />
+                منوی سایت
+              </button>
+            </li>
+            <li>
+              <button className="headerLinkBtn">
+                <MdKeyboardArrowDown />
+                دسته بندی
+              </button>
             </li>
             <li className="headerLink">
               <Link className="headerLinkItem">رویداد ها</Link>
@@ -36,10 +54,39 @@ const Header = () => {
         </div>
 
         <div className="countDown">
-          <div>00:00:00:00</div>
+          <div className="counterItem days">
+            <span className="countDownNum">00</span>
+            <span className="countDownLabel">روز</span>
+          </div>
+          <div className="counterItem hours">
+            <span className="countDownNum">00</span>
+            <span className="countDownLabel">ساعت</span>
+          </div>
+          <div className="counterItem minutes">
+            <span className="countDownNum">00</span>
+            <span className="countDownLabel">دقیقه</span>
+          </div>
+          <div className="counterItem seconds">
+            <span className="countDownNum">00</span>
+            <span className="countDownLabel">ثانیه</span>
+          </div>
         </div>
 
-        <div className="headerIcons"></div>
+        <div className="headerIcons">
+          <button>
+            <FaShoppingCart />
+          </button>
+          <button>
+            <FaUser />
+          </button>
+          <button onClick={() => dispatch(toggle(!theme))}>
+            {theme === true ? (
+              <img src={moonIcon} alt="" />
+            ) : (
+              <img src={sunIcon} alt="" />
+            )}
+          </button>
+        </div>
       </header>
     </div>
   );
--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
+import { useSelector } from "react-redux";
 
 const AppLayout = () => {
+  const theme = useSelector((state) => state.theme.dark);
+
   return (
     <>
       <Header />
 
-      <main>
+      <main className={theme ? "main dark" : "main light"}>
         <Outlet />
       </main>
 
