4e92defa7dada7ef87f38a72ae6dd701d931a79e
Behnam Hamidi <eslp91@hotmail.com>
2024-03-31 08:00:27 +0330

    remove all

--- src/components/shared/ReadBtn/Button.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./readBtn.css";
-
-const ReadBtn = ({ children }: any) => {
-  return <button className="readBtn">{children}</button>;
-};
-
-export default ReadBtn;
