33e05809d3b4737c0f963417ac7afbfa6534a2e9
Behnam Hamidi <eslp91@hotmail.com>
2024-04-01 12:29:30 +0330

    grid done

--- src/components/Homepage/Grid/Grid.jsx
+++ src/components/Homepage/Grid/Grid.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./grid.css";
 import { courses } from "../../../data";
+import { Link } from "react-router-dom";
+import { CiHeart } from "react-icons/ci";
+import { FaHeart } from "react-icons/fa";
+import { CiBookmark } from "react-icons/ci";
+import { FaBookmark } from "react-icons/fa";
+import { IoIosShareAlt } from "react-icons/io";
 
 const Grid = () => {
   const selectedIds = [19, 3, 14, 20, 6, 9];
@@ -19,7 +25,18 @@ const Grid = () => {
         {filteredCourses.map((item) => (
           <div key={item.id} className="gridHomepage-box">
             <img src={item.image} alt="" />
-            <div className="gridHomepage-imageContent"></div>
+            <div className="gridHomepage-content">
+              <h2>{item.title}</h2>
+              <p>{item.content.substring(0, 70)} ...</p>
+              <Link to="/" className="gridHomepage-ReadBtn">
+                جزئیات بیشتر
+              </Link>
+              <div className="gridHomepage-iconContainer">
+                <IoIosShareAlt className="gridHomepage-icon" />
+                <CiBookmark className="gridHomepage-icon" />
+                <CiHeart className="gridHomepage-icon" />
+              </div>
+            </div>
           </div>
         ))}
       </div>
