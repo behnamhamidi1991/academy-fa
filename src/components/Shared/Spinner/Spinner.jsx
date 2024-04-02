a3f1b298c0ee7fdd27b5dbf8e723b2755379e052
Behnam Hamidi <eslp91@hotmail.com>
2024-04-02 23:40:43 +0330

    go

---
+++ src/components/Shared/Spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import spinner from "../../../assets/loadSpinner.svg";
+
+const Spinner = () => {
+  return (
+    <div>
+      <img src={spinner} alt="" />
+    </div>
+  );
+};
+
+export default Spinner;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,5 @@
 import React, { useEffect } from "react";
-import image1 from "../../assets/images/barista.jpg";
-import image2 from "../../assets/images/js.jpg";
-import image3 from "../../assets/images/kotlin.jpg";
-import image4 from "../../assets/images/sp.jpg";
-import image5 from "../../assets/images/guitar.jpg";
-import image6 from "../../assets/images/en.jpg";
-import { CiHeart } from "react-icons/ci";
-import { IoLogoWhatsapp } from "react-icons/io";
-import { AiFillInstagram } from "react-icons/ai";
+
 import "react-loading-skeleton/dist/skeleton.css";
 import "./home.css";
 import Story from "../../components/Homepage/Story/Story";
